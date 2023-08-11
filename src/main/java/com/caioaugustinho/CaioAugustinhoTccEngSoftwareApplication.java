011e1d775006b970d88b86c1d41de5b2bf150cab
caioaugustinho <caioaugustinhodonascimento@gmail.com>
2023-08-11 11:36:25 -0300

    atuall

--- src/main/java/com/caioaugustinho/CaioAugustinhoTccEngSoftwareApplication.java
+++ src/main/java/com/caioaugustinho/CaioAugustinhoTccEngSoftwareApplication.java
@@ -1,23 +1,3 @@
-/**
-  * Pacote referente a implementação do Trabalho de Conclusão de Curso (TCC) 
-  * do curso de especialização em  Engenharia de Software da PUC Minas Virtual. 
-  * 
-  * Data de Entrega = 31/Maio/2020
-  * 
-  * O TCC parte de um cenário de demanda de desenvolvimento de um sistema para
-  * uma Academia de Ginástica. As descrições deste contexto contem requisitos
-  * de software que serão considerados em todo o trabalho, sendo papel do aluno
-  * entender e refinar os requisitos fornecidos. Sugere-se fortemente ao aluno 
-  * procurar conhecer sistemas semelhantes ao que irá escolher, a título de 
-  * inspiração. O objetivo do TCC é realizar a modelagem e especificação do 
-  * sistema escolhido, bem como produzir outros artefatos relacionados à 
-  * Engenharia de Software.
-  * 
-  * @author 	Caio Augustinho
-  * Email: 	<a href="mailto:caioaugustinho@gmail.com">caioaugustinho@gmail.com</a>
-  * Linkedin:   <a href="https://www.linkedin.com/in/caioaugustinhoferreira">Link</a>
-  * Lattes:	<a href="http://lattes.cnpq.br/3613833986194000">Link</a>    
-  */
 
 package com.caioaugustinho;
 import org.springframework.boot.SpringApplication;
